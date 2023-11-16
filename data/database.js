85a6397baef40c35a3d7c222affd73898ddb96f6
Jitendra Rawat <120551529+jitendra-rawat@users.noreply.github.com>
2023-11-16 23:20:23 +0530

    [added mongo db ]

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
 
- mongoose.connect(process.env.MONGO_URL, {
+ mongoose.connect(process.env.MONGO_URI, {
    
 })
 .then( () => console.log("Database connected") )
