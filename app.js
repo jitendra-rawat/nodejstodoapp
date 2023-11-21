381142bdabf3bd6bb41211d090035eb951daee69
Jitendra Rawat <120551529+jitendra-rawat@users.noreply.github.com>
2023-11-21 22:59:24 +0530

    [removed credationals]

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(cors({
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
+    credentials: false,
 }));
 
 
