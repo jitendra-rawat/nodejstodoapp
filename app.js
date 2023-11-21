cf05162e1239e7b788cea5d16f082f03e33ba6b6
Jitendra Rawat <120551529+jitendra-rawat@users.noreply.github.com>
2023-11-21 22:33:54 +0530

    [added domain name]

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ config({
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.DOMAIN_NAME,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
