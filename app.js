8ba0547e0ff863b62d60fd8a9fdc0491964a8511
Jitendra Rawat <120551529+jitendra-rawat@users.noreply.github.com>
2023-11-21 21:39:25 +0530

    [fixed the cors]

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ config({
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin:http://localhost:5173,
-    methods:["GET", "POST","PUT","DELETE"],
-    credentials:true,
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
 }));
 
 
