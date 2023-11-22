c6f0fe73c7c21ca8c506bc03f195fc9c203e32e4
Jitendra Rawat <120551529+jitendra-rawat@users.noreply.github.com>
2023-11-22 23:35:22 +0530

    [cors]

--- app.js
+++ app.js
@@ -1,55 +1,36 @@
-import express from 'express'
-import {config} from "dotenv"
-import userRouter from './routes/user.js'
-import taskRouter from './routes/task.js'
-import cookieParser from 'cookie-parser';
-import { errorMiddleware } from './middlewares/error.js';
-import cors from 'cors'
+import express from "express";
+import { config } from "dotenv";
+import userRouter from "./routes/user.js";
+import taskRouter from "./routes/task.js";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
 
-export  const app = express();
+export const app = express();
 
 config({
-    path:"./config.env"
-})
-
-
+  path: "./config.env",
+});
 
 //using middleware
 app.use(express.json());
-app.use(cookieParser())
-app.use(cors()); 
+app.use(cookieParser());
 
-// app.use(cors({
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
 
-//      origin:'http://localhost:5173',
-
-   
-//     methods:["GET","POST","PUT","DELETE"],
-//     credentials:true
-// }));
-
-
-
-
-
-
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 //Using routes
-app.use("/api/v1/users" ,userRouter)
-app.use("/api/v1/task" ,taskRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
 
+app.get("/", (req, res) => {
+  res.send("Hello, Backend is Working");
+});
 
-app.get("/" , (req, res) => {
-
-    res.send("Hello, Backend is Working")
-
-
-})
-
-
-app.use( errorMiddleware )
-
-
-
-
-
+app.use(errorMiddleware);
