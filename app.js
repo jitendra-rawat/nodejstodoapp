290eb1fc9d10ae338c6d7d2aacec244b891cf8ec
Jitendra Rawat <120551529+jitendra-rawat@users.noreply.github.com>
2023-11-22 22:07:09 +0530

    [CORS updated ]

--- app.js
+++ app.js
@@ -23,6 +23,15 @@ app.use(cookieParser())
 //     credentials: true,
 // }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
+
+
 
 
 //Using routes
