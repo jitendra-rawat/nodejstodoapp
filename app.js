9c99a42e15d1ecea45633f695e9f533e975a535d
Jitendra Rawat <120551529+jitendra-rawat@users.noreply.github.com>
2023-11-22 22:22:56 +0530

    [cors problem]

--- app.js
+++ app.js
@@ -40,10 +40,9 @@ app.get("/" , (req, res) => {
     res.send("Hello, Backend is Working")
 
     res.setHeader("Access-Control-Allow-Origin", "*")
-res.setHeader("Access-Control-Allow-Credentials", "true");
-res.setHeader("Access-Control-Max-Age", "1800");
-res.setHeader("Access-Control-Allow-Headers", "content-type");
-res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
 
 })
 
